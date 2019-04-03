96a700a47ed13b30886bf9a2506553aa15bafb5b
Ivan Kurtev <ikurtev@hotmail.com>
2019-04-03 17:19:41 -0400

    -merging from origin

--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/PluginRegistration/Step.cs
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/PluginRegistration/Step.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Xrm.Framework.CI.PowerShell.Cmdlets.Common;
+using System.Linq;
+using Xrm.Framework.CI.Common.Entities;
 
 namespace Xrm.Framework.CI.PowerShell.Cmdlets
 {
@@ -30,6 +31,10 @@ namespace Xrm.Framework.CI.PowerShell.Cmdlets
 
         public SdkMessageProcessingStep_SupportedDeployment? SupportedDeployment { get; set; }
 
+        public bool? AsyncAutoDelete { get; set; }
+
+        public SdkMessageProcessingStepState? StateCode { get; set; }
+
         public List<Image> Images { get; set; }
 
         public override int GetHashCode()
@@ -46,7 +51,58 @@ namespace Xrm.Framework.CI.PowerShell.Cmdlets
             hashCode = hashCode * -1521134295 + EqualityComparer<int?>.Default.GetHashCode(Rank);
             hashCode = hashCode * -1521134295 + EqualityComparer<SdkMessageProcessingStep_Stage?>.Default.GetHashCode(Stage);
             hashCode = hashCode * -1521134295 + EqualityComparer<SdkMessageProcessingStep_SupportedDeployment?>.Default.GetHashCode(SupportedDeployment);
+            hashCode = hashCode * -1521134295 + EqualityComparer<bool?>.Default.GetHashCode(AsyncAutoDelete);
             return hashCode;
         }
+
+        public static Step operator +(Step b, Step c)
+        {
+            var step = new Step
+            {
+                Id = c.Id,
+                Name = b.Name,
+                MessageName = b.MessageName,
+                Description = b.Description,
+                CustomConfiguration = b.CustomConfiguration,
+                FilteringAttributes = b.FilteringAttributes,
+                ImpersonatingUserFullname = b.ImpersonatingUserFullname,
+                Mode = b.Mode,
+                PrimaryEntityName = b.PrimaryEntityName,
+                Rank = b.Rank,
+                Stage = b.Stage,
+                SupportedDeployment = b.SupportedDeployment,
+                AsyncAutoDelete = b.AsyncAutoDelete,
+                StateCode = b.StateCode,
+                Images = new List<Image>()
+            };
+
+            if (b.Images == null) return step;
+
+            foreach (var image in b.Images)
+            {
+                var original = b.Images.First(x => x.SameAsRegistered(image));
+                var corresponding = c.Images?.FirstOrDefault(x => x.SameAsRegistered(image));
+                if (corresponding != null)
+                {
+                    original += corresponding;
+                }
+                step.Images.Add(original);
+            }
+
+            return step;
+        }
     }
+
+    public static class StepExtensions
+    {
+        public static bool SameAsRegistered(this Step original, Step compare)
+        {
+            return original != null
+                   && (compare != null
+                       && original.Name == compare.Name
+                       && original.PrimaryEntityName == compare.PrimaryEntityName
+                       && original.Stage == compare.Stage);
+        }
+    }
+
 }
--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/SetXrmServiceEndpointRegistration.cs
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/SetXrmServiceEndpointRegistration.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Management.Automation;
+using Xrm.Framework.CI.Common;
+using Xrm.Framework.CI.Common.Entities;
 using Xrm.Framework.CI.PowerShell.Cmdlets.Common;
 using Xrm.Framework.CI.PowerShell.Cmdlets.PluginRegistration;
 
@@ -55,6 +57,10 @@ namespace Xrm.Framework.CI.PowerShell.Cmdlets
                 {   
                     var serviceEndptLst = ReadServiceEndpointMappingFile(MappingFile);
                     pluginRegistrationHelper.UpsertServiceEndpoints(serviceEndptLst, SolutionName, RegistrationType);
+                }
+                else
+                {
+                    WriteError(new ErrorRecord(new ArgumentException("No operation performed. Mapping File not exists."), "FileMissing", ErrorCategory.InvalidArgument, MappingFile));
                 }
             }
 
